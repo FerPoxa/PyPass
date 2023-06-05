076220ce1aab936baeee6dff906adcb625943c44
FerPoxa <113585786+FerPoxa@users.noreply.github.com>
2023-06-05 15:50:30 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import string
+import random
+import click
+
+try:
+    @click.command()
+    @click.option('-l', '--length', prompt="Enter the length of your password")
+    def generate(length):
+        if int(length) <0:
+            pass
+        characters = string.ascii_letters + string.punctuation
+        mylist = []
+        for i in range(int(length)):
+            mylist.append(random.choice(list(characters)))
+        password = ''.join(mylist)
+        print(password)
+
+    if __name__ == '__main__':
+        generate()
+except ValueError:
+    print('\033[31m You must enter a integer number.')
+except KeyboardInterrupt:
+    print('\033[31m Script stopped\n')
