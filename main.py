482379381d3d8ed1b2f2892e1a3018a37de41927
FerPoxa <113585786+FerPoxa@users.noreply.github.com>
2024-01-21 14:23:20 +0100

    Update main.py
    Updated script to use all ascii characters

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import string
-import random
 import click
+import random
 
 try:
     @click.command()
     @click.option('-l', '--length', prompt="Enter the length of your password")
     def generate(length):
-        characters = string.ascii_letters + string.punctuation
+        characters = list(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
         mylist = []
         for i in range(int(length)):
-            mylist.append(random.choice(list(characters)))
+            mylist.append(random.choice(characters))
         password = ''.join(mylist)
         print(password)
-
     if __name__ == '__main__':
         generate()
 except ValueError:
     print('\033[31m You must enter a integer number.')
 except KeyboardInterrupt:
     print('\033[31m Script stopped\n')
+    
